5a599102bfcd851a31698add8587cc15065e0bc7
Anton Fifindik <fifindik.av@gmail.com>
2018-03-26 00:41:30 +0300

    init commit

---
+++ src/main/java/com/antonfifindik/discographies/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.antonfifindik.discographies.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Антон on 25.03.2018.
+ */
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "index";
+    }
+
+}
---
+++ web/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Антон
+  Date: 25.03.2018
+  Time: 23:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+    <h3>Hello World!</h3>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
